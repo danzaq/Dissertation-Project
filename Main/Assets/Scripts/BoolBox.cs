eb9c33f8e830b621119eda484f2feb97c9810a6b
danzaq <34459944+danzaq@users.noreply.github.com>
2019-05-08 00:05:56 +0200

    line renderer change of material

--- Main/Assets/Scripts/BoolBox.cs
+++ Main/Assets/Scripts/BoolBox.cs
@@ -5,14 +5,37 @@ using UnityEngine;
 public class BoolBox : MonoBehaviour
 {
     public bool value = false;
+    public LineRenderer line;
+    public bool switched;
 
     public InteractiveObject[] objects;
 
+    void Start()
+    {
+        
+    }
+    void Update() 
+    {
+        Debug.Log(switched);
+        if (switched)
+        {
+            line.material = line.materials[1];
+        }
+        else if(!switched)
+        {
+            line.material = line.materials[0];
+        }
+    }
     public void Trigger()
     {
         foreach (InteractiveObject obj in objects)
         {
             obj.Affect(value);
         }
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        switched = true;
     }
 }
